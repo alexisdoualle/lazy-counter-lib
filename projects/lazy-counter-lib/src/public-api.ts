0ca2a4d4a854719bee5c0c482511d4a8d5742234
alexis <alexis.doualle@wezen.com>
2019-11-18 16:07:44 +0100

    angular 7 rollback

--- projects/lazy-counter-lib/src/public-api.ts
+++ projects/lazy-counter-lib/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of lazy-counter-lib
  */
 
-export * from './lib/lazy-counter-lib.service';
 export * from './lib/lazy-counter-lib.component';
 export * from './lib/lazy-counter-lib.module';
